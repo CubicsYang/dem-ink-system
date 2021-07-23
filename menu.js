9035a664191499ae8cc1b9d242cbc09469cf9bc3
Cubics <767723217@qq.com>
2021-07-23 17:05:59 +0800

    first version

---
+++ menu.js
@@ -0,0 +1,56 @@
+const template = [
+    // {
+    //     label: '文件',
+    //     submenu: [{
+    //             label: '关于',
+    //             role: 'about'
+    //         },
+    //         {
+    //             type: 'separator'
+    //         },
+    //         {
+    //             label: '关闭',
+    //             accelerator: 'Command+Q',
+    //             click: () => {
+    //                 mainWindow.close();
+    //             }
+    //         }
+    //     ]
+    // },
+    // {
+    //     label: '编辑',
+    //     submenu: [{
+    //             label: '复制',
+    //             click: () => {
+    //                 console.log('复制');
+    //             }
+    //         },
+    //         {
+    //             label: '剪切',
+    //             click: () => {
+    //                 console.log('剪切');
+    //             }
+    //         },
+    //         {
+    //             type: 'separator'
+    //         },
+    //         {
+    //             label: '查找',
+    //             accelerator: 'Command+F',
+    //             click: () => {
+    //                 console.log('查找');
+    //             }
+    //         },
+    //         {
+    //             label: '替换',
+    //             accelerator: 'Command+R',
+    //             click: () => {
+    //                 console.log('替换');
+    //             }
+    //         }
+
+    //     ]
+    // }
+];
+
+module.exports = template
